9fc04544f25525cdc0124286e3380383b1221534
hakula <jsph.niu@gmail.com>
2017-09-08 04:53:09 -0600

    Models & Migrations
    All models, migrations and sessions table along with auth routes

--- database/migrations/2017_09_08_104324_create_applicants_table.php
+++ database/migrations/2017_09_08_104324_create_applicants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobsTable extends Migration
+class CreateApplicantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,16 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('applicants', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('job_id')->unsigned();
             $table->string('name');
+            $table->string('email');
+            $table->string('website');
+            $table->text('cover_letter');
             $table->timestamps();
+            
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
         });
     }
 
@@ -27,6 +33,6 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('applicants');
     }
 }
--- database/migrations/2017_09_08_104705_create_skills_table.php
+++ database/migrations/2017_09_08_104705_create_skills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobsTable extends Migration
+class CreateSkillsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('skills', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('applicant_id')->unsigned();
             $table->string('name');
             $table->timestamps();
         });
@@ -27,6 +28,6 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('skills');
     }
 }
