cac0da842559391f5b51a420df06b21c2635dfae
hakula <jsph.niu@gmail.com>
2017-09-08 12:44:21 -0600

    Sample Data and Factory content

--- database/factories/ApplicantFactory.php
+++ database/factories/ApplicantFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+		'name' => $faker->name,
+		'email' => $faker->email,
+		'website' => $faker->url,
+		'cover_letter' => $faker->paragraph(),
     ];
 });
--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+		'name' => collect(config('skills'))->random(),
     ];
 });
