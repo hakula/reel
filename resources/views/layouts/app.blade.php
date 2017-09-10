fc0b77c73b46a4b1d52bb54e38b8fc0ee74a9dfc
hakula <jsph.niu@gmail.com>
2017-09-09 21:06:32 -0600

    Update home link
    Update home link in navbar to point to home if authenticated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
                     </button>
 					
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="@guest {{ url('/') }} @else {{ url('/home') }} @endguest">
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
